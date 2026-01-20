b5809759b563eb72a962690e8870cefc0d32f794
AlexJ2006 <alexjakeman@sky.com>
2026-01-20 15:55:00 +0000

    Beginning to work with NUMpy

--- Lab1.py
+++ Lab1.py
@@ -111,6 +111,31 @@ AI_Students = [["Alice", 85], ["Bob", 92], ["Charlie", 78]]
 
 
 #----------------------------------------------------------- BEGIN WORKING WITH NUMpy
+#Instructions on sheet 1.3, excercise at the bottom of the sheet.
+import numpy as np
+
+#Setting the lists containing the data for modules 1 and 2
+student_data_mod1 = np.array([45, 60, 75, 30, 50])
+student_data_mod2 = np.array([45, 60, 75, 30, 50])
+
+#Defining a function to perform min max normalisation
+def min_max1(arr): #For the first module
+    min_val1 = np.min(arr)
+    max_val1 = np.max(arr)
+    return(arr-min_val1)/(max_val1-min_val1)
+
+def min_max2(arr): #For the second module
+    min_val2 = np.min(arr)
+    max_val2 = np.max(arr)
+    return()
+
+module1_normalisation = min_max1(student_data_mod1)
+module2_normalisation = min_max2(student_data_mod2)
+
+print(module1_normalisation)
+print(module2_normalisation)
+
+
 
 
 
